d59cd805c539bd1b39f73b5df71465974e83dd32
Roman Behroz <romanbrkz@live.com>
2023-02-10 15:34:01 +0100

    KBE-49 Swagger Documentation OpenAPI implemented

--- src/main/java/com/onlineshop/cart/Application.java
+++ src/main/java/com/onlineshop/cart/Application.java
@@ -1,9 +1,18 @@
 package com.onlineshop.cart;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Cart Microservice", version = "1.0.0"),
+//		servers = {@Server(url = "http://localhost:8080"), @Server(url = "http://example.com")},
+		tags = {@Tag(name = "Cart", description = "This is Cart Microservice End Points ")}
+)
 class Application{
 
 	public static void main(String[] args) {
